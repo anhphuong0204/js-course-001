00f2a59ca5abf8bf410c5e0bd8f2c64012a0b08c
anhphuong0204 <47697482+anhphuong0204@users.noreply.github.com>
2021-01-23 02:33:16 +0700

    Return to client a html file

--- create-a-server/server.js
+++ create-a-server/server.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url);
+    console.log(req.method); // log req object to console
+    // Response a file to client from server we have made
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('./view/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            res.end(data);
+        }
+    })
 })
 
 server.listen(3000, () => {
