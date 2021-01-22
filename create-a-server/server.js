307a1ba2d75f1d9ab5bee5d10df2574d304c2bdc
anhphuong0204 <47697482+anhphuong0204@users.noreply.github.com>
2021-01-23 02:43:22 +0700

    Routing with http module

--- create-a-server/server.js
+++ create-a-server/server.js
@@ -4,9 +4,24 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     console.log(req.method); // log req object to console
+
+
+    let route = './view/';
+    switch (req.url) {
+        case '/':
+            route += 'index.html';
+            break;
+        case '/contact':
+            route += 'contact.html';
+            break;
+        default:
+            route += '404.html';
+            break;
+    }
+
     // Response a file to client from server we have made
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./view/index.html', (err, data) => {
+    fs.readFile(route, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
